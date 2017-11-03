cda696f2cc8002b75b2cdc04755483c7a45b11e4
Lawrence <LAWRENCX@legal.regn.net>
2017-11-03 16:28:13 +0000

    first commit

---
+++ src/CLHomepage/Controllers/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+
+//// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace CLHomepage.Controllers
+//{
+//    public class HelloWorldController : Controller
+//    {
+//        // GET: /<controller>/
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
